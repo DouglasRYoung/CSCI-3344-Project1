1e62f4edda550421e0ddf0cc9ab5ec7adfa0d9f8
Douglas <dyoung2@trinity.edu>
2021-05-02 15:50:24 -0500

    Questions 1 and 2 done

--- reinforcement/valueIterationAgents.py
+++ reinforcement/valueIterationAgents.py
@@ -204,5 +204,52 @@ class PrioritizedSweepingValueIterationAgent(AsynchronousValueIterationAgent):
 
     def runValueIteration(self):
         "*** YOUR CODE HERE ***"
+        #Step 1 is to initiate Priority Queue 
+        thePriorityQ = util.PriorityQueue()
+        #Step 2 is to have predeccesors 
+        predecessors = {}
+        #Go Through Each State
+        for state in self.mdp.getStates():
+          if not self.mdp.isTerminal(state):
+            for action in self.mdp.getPossibleActions(state):
+              for nextState, prob in self.mdp.getTransitionStatesAndProbs(state, action):
+                if nextState in predecessors:
+                  predecessors[nextState].add(state)
+                else:
+                  predecessors[nextState] = {state}
+
+        for state in self.mdp.getStates():
+          if not self.mdp.isTerminal(state):
+            values = []
+            for action in self.mdp.getPossibleActions(state):
+              q_value = self.computeQValueFromValues(state, action)
+              values.append(q_value)
+            diff = abs(max(values) - self.values[state])
+            thePriorityQ.update(state, - diff)
+
+        for i in range(self.iterations):
+          if thePriorityQ.isEmpty():
+            break
+          temp_state = thePriorityQ.pop()
+          if not self.mdp.isTerminal(temp_state):
+            values = []
+            for action in self.mdp.getPossibleActions(temp_state):
+              q_value = self.computeQValueFromValues(temp_state, action)
+              values.append(q_value)
+            self.values[temp_state] = max(values)
+
+          for p in predecessors[temp_state]:
+            if not self.mdp.isTerminal(p):
+              values = []
+              for action in self.mdp.getPossibleActions(p):
+                q_value = self.computeQValueFromValues(p, action)
+                values.append(q_value)
+              diff = abs(max(values) - self.values[p])
+              if diff > self.theta:
+                thePriorityQ.update(p, -diff)
+
+                            
+
+
         
 
